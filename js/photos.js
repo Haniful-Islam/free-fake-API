11cae3c5b0428b5f5c758a6e89e1308da8534b20
Haniful Islam <haniful360@gmail.com>
2022-11-06 20:45:19 +0600

    photos added

---
+++ js/photos.js
@@ -0,0 +1,25 @@
+function handleComments() {
+  fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((res) => res.json())
+    .then((data) => displayComments(data));
+}
+
+function displayComments(photos) {
+  const commentContainer = document.getElementById("comments-container");
+  commentContainer.classList.add('photos')
+  for (const photo of photos) {
+    const div = document.createElement("div");
+    div.classList.add('photo')
+    const h2 = document.createElement("h2");
+    div.appendChild(h2);
+    const p = document.createElement("p");
+    div.appendChild(p);
+    const img = document.createElement("img");
+    img.classList.add('img-width');
+    div.appendChild(img);
+    img.src = photo.url;
+    div.innerHTML = `<h2>${photo.title}</h2>`
+    commentContainer.appendChild(img);
+    commentContainer.appendChild(div);
+  }
+}
